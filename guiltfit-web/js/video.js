49f91ec45c38f782f3160197fff371717ab46fa0
Sara <saraclay@usc.edu>
2015-04-09 14:11:14 -0700

    GuiltFit Project

---
+++ guiltfit-web/js/video.js
@@ -0,0 +1,25 @@
+var vid = document.getElementById("bgvid");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
